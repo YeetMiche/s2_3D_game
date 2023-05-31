be1b36083bce8552315bb90cfed06c4c5d9a799f
YeetMiche <64234954+YeetMiche@users.noreply.github.com>
2023-05-31 14:37:00 +0200

    added controlls to external header file

---
+++ controlls.hpp
@@ -0,0 +1,56 @@
+extern ButtonKeys Keys;
+
+void buttons_up(unsigned char key, int x, int y){
+	if (key=='a'){Keys.a=0;}
+	if (key=='d'){Keys.d=0;}
+	if (key=='w'){Keys.w=0;}
+	if (key=='s'){Keys.s=0;}
+
+	if (key=='i'){Keys.up=0;}
+	if (key=='k'){Keys.down=0;}
+
+	if (key=='u'){Keys.space=0;}
+	if (key=='m'){Keys.lctrl=0;}
+
+	if (key=='r'){Keys.pfov=0;}
+	if (key=='t'){Keys.mfov=0;}
+}
+
+void buttons_down(unsigned char key, int x, int y){
+	if (key=='a'){Keys.a=1;}
+	if (key=='d'){Keys.d=1;}
+	if (key=='w'){Keys.w=1;}
+	if (key=='s'){Keys.s=1;}
+
+	if (key=='i'){Keys.up=1;}
+	if (key=='k'){Keys.down=1;}
+
+	if (key=='u'){Keys.space=1;}
+	if (key=='m'){Keys.lctrl=1;}
+
+	if (key=='r'){Keys.pfov=1;}
+	if (key=='t'){Keys.mfov=1;}
+}
+
+void mouse_func(int x, int y){
+	if (x <= 0){glutWarpPointer(window_x * render_scale, y);}
+	if (x >= window_x * render_scale - 1){glutWarpPointer(0, y);}
+	P.a = float(x) / (render_scale * window_x) * 360;
+	glutSetCursor(GLUT_CURSOR_NONE);
+}
+
+
+void move_player() {
+	int dx = sin(P.a/180*M_PI) * 10.0;
+	int dy = cos(P.a / 180 * M_PI) * 10.0;
+	if (Keys.w == 1) { P.x += dx; P.y += dy; }
+	if (Keys.s == 1) { P.x -= dx; P.y -= dy; }
+	if (Keys.a == 1) { P.x -= dy/2; P.y += dx/2; }
+	if (Keys.d == 1) { P.x += dy/2; P.y -= dx/2; }
+	if (Keys.down == 1) { P.l -= 1; }
+	if (Keys.up == 1) { P.l += 1; }
+	if (Keys.lctrl == 1) { P.z += 1; }
+	if (Keys.space == 1) { P.z -= 1; }
+	if (Keys.pfov == 1) { FOV += 1; }
+	if (Keys.mfov == 1) { FOV -= 1; }
+}
