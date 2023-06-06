90d30b85db036ecb92fa22bd663d3109c1c73de5
YeetMiche <64234954+YeetMiche@users.noreply.github.com>
2023-06-06 10:45:06 +0200

    added texture converting tool

--- texture_converter.py
+++ texture_converter.py
@@ -1,10 +1,10 @@
 import cv2 as cv
 
-img = cv.imread("doomTiles/STONE2.png",0)
+img = cv.imread("test.png",0)
 
-file = open("texture.bmp", "w")
+file = open("test.bmp", "w")
 
-for y in range(128):
-    for x in range(128):
+for y in range(16):
+    for x in range(16):
         file.write(str(img[x,y]) + ",")
 print(img[0,0])
